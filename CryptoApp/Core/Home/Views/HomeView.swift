e192cc829da96880bf5899c0accea27910e6344f
NamaN <naman@NamaNs-MacBook-Air.local>
2023-09-17 03:10:39 +0530

    Completed NavigationLink to Coin's DetailView

--- CryptoApp/Core/Home/Views/HomeView.swift
+++ CryptoApp/Core/Home/Views/HomeView.swift
@@ -13,6 +13,9 @@ struct HomeView: View {
     @State private var showPortfolio : Bool = true // animate right
     @State var showPortfolioView : Bool = false // new sheet
     
+    @State private var selectedCoin : CoinModel? = nil
+    @State private var showDetailedView : Bool = false
+    
     var body: some View {
         ZStack{
             // background layer
@@ -47,6 +50,13 @@ struct HomeView: View {
                 Spacer(minLength: 0)
             }
         }
+        .background(
+            NavigationLink(isActive: $showDetailedView, destination: {
+                DetailLoadingView(coin: $selectedCoin)
+            }, label: {
+                
+            })
+        )
     }
 }
 
@@ -97,6 +107,9 @@ extension HomeView {
             ForEach(vm.allCoins) { coin in
                 CoinRowView(coin: coin, showHoldingsColumn: false)
                     .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
+                    .onTapGesture{
+                        segue(coin: coin)
+                    }
             }
         }
         .listStyle(PlainListStyle())
@@ -107,9 +120,18 @@ extension HomeView {
             ForEach(vm.portfolioCoins) { coin in
                 CoinRowView(coin: coin, showHoldingsColumn: true)
                     .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
+                    .onTapGesture {
+                        segue(coin: coin)
+                    }
             }
         }
         .listStyle(PlainListStyle())
+    }
+    
+    private func segue(coin : CoinModel){
+        selectedCoin = coin
+        showDetailedView.toggle()
+        showPortfolio.toggle()
     }
 
     private var columnTitles : some View{
